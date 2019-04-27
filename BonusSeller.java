03fbd9599f1bb90fae0c5012dc0885398d486de0
mairne <43656838+mairne@users.noreply.github.com>
2019-04-27 16:21:07 +0100

    Add files via upload

--- BonusSeller.java
+++ BonusSeller.java
@@ -5,7 +5,7 @@ private HashMap<String,Double> sales;
 
 	public BonusSeller(String n) {
 		super(n);
-		sales = new HashMap<String,Double>(10);
+		sales = new HashMap<String, Double> (10);
 	}
 
 	public void sale(String name, double number) {
@@ -18,12 +18,25 @@ private HashMap<String,Double> sales;
 		String output = super.toString() + "\n";
 		output += "Sales per customer: \n";
 		for(int i = 0; i < sales.size(); i++) {
-		outptut += sales.keyset().toArray[i]+ ":" + sales.get(i) + "kg \n";
+		output += sales.keySet().toArray()[i] + ":" + sales.get(i) + "kg \n";
 		}
+		return output;
 	}
 	
 	public int calculateBonus() {
-		return 0;
+		int result = -1;
+		for(int i = 0; i < sales.size(); i++) {
+		if(sales.get(i) < 20) {
+			sales.remove(i);
+		}
 	}
-
+		if(sales.values().size() < 5) {
+			result = 0;
+		}else if(sales.values().size() < 10 || sales.values().size() >= 5) {
+			result = 5;
+		}else if(sales.values().size() >= 10) {
+			result = 7;
+		}
+		return result;
+	}
 }
