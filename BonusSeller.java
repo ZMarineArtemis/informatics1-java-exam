804aacee8387b4efdd7bdc288adcbe546c85b556
mairne <43656838+mairne@users.noreply.github.com>
2019-04-27 15:48:02 +0100

    Add files via upload

--- BonusSeller.java
+++ BonusSeller.java
@@ -1,4 +1,30 @@
+import java.util.HashMap;
 
-public class BonusSeller {
+public class BonusSeller extends Seller {
+private HashMap<String,Double> sales;
+
+	public BonusSeller(String n) {
+		super(n);
+		int capacity = sales.size();
+		capacity = 10;
+	}
+
+	public void sale(String name, double number) {
+		super.sale(number);
+		sales.put(name, number);		
+	}
+
+	
+	public String toString() {
+		String output = super.toString() + "\n";
+		output += "Sales per customer: \n";
+		for(int i = 0; i < sales.size(); i++) {
+		outptut += sales.+ ":" + sales.get(i) + "kg \n";
+		}
+	}
+	
+	public int calculateBonus() {
+		return 0;
+	}
 
 }
